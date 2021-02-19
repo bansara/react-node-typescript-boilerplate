992f31e22b0e0ec5735a39bb7a59d750241aa914
Joshua Geisler <joshuageisler@Joshuas-MBP.home>
2021-02-19 10:24:51 -0500

    initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom';
-import Rando from './components/randoModule';
-import './app.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+} from "react-router-dom";
+import HelloWorld from "./components/HelloWorld";
+import "./app.css";
 
 const App: React.FC = () => {
   return (
-    <Router
-      basename="/"
-    >
+    <Router basename="/">
       <Switch>
-        <Route 
-          exact 
-          path="/" 
-          component={Rando} 
-        />
+        <Route exact path="/" component={HelloWorld} />
         <Redirect to="/" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-const app: HTMLElement = document.getElementById('app')!;
+const app: HTMLElement = document.getElementById("app")!;
 ReactDOM.render(<App />, app);
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld: React.FC = () => {
+  return <h1>Hello React, Typescript, Express, and Webpack</h1>;
+};
+
+export default HelloWorld;
